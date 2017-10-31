206dd4b40317cfbda672791ea9a46c0d8b53ade6
SethEggerRocks <setheggerrocks@gmail.com>
2017-10-30 20:29:41 -0400

    Create process.php

---
+++ process.php
@@ -0,0 +1,30 @@
+<html>
+
+
+<head>
+
+
+<title>Hmmm</title>
+
+
+</head>
+
+
+<body>
+
+
+<?php
+ 
+	$name = $_POST["uname"];
+	echo $name;
+	echo " ";
+	$name2 = $_POST["upassword"];
+	echo $name2;
+	
+?>
+
+
+</body>
+
+
+</html>
